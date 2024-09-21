ed179af3e7dd506f62eb7952998a0d7612cdf133
Hlynur Isak <isakvillason@hotmail.com>
2024-09-21 12:32:29 +0000

    start puzzlesolver

---
+++ puzzlesolver.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstring>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstdlib>
+#include <cerrno>
+#include <sys/time.h>
+
+#define BUFFER_SIZE 1024
+
+using namespace std;
+
+
+int main(int argc, char *argv[]) {
+
+    // Check for correct number of arguments
+    if (argc != 6) {
+        cerr << "Usage: ./puzzlesolver <IP address> <port1> <port2> <port3> <port4>" << endl;
+        exit(1);
+    }
+    
+    // Parse arguments
+    const char *ip_string = argv[1];
+    int port1 = atoi(argv[2]);
+    int port2 = atoi(argv[3]);
+    int port3 = atoi(argv[4]);
+    int port4 = atoi(argv[5]);
+
+}
